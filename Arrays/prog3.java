43bd8f7e2300229c755b2fd47a1c3e5f03d50467
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-09 00:18:12 +0530

    3nd prog

--- Arrays/prog3.java
+++ Arrays/prog3.java
@@ -1,6 +1,8 @@
 package Arrays;
 // Count Pairs Whose XOR Is Even
 
+import java.util.Scanner;
+
 // Input:  [1, 2, 3, 4]
 // Pairs:
 // (1,3) → odd ^ odd = even
@@ -9,5 +11,31 @@ package Arrays;
 // Output: 2
 
 public class prog3 {
+    public static int result(int[] arr) {
+        int ce = 0;
+        int co = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                ce += 1;
+            } else {
+                co += 1;
+            }
+        }
+        int evpairs = ce * (ce - 1) / 2;
+        int odpairs = co * (co - 1) / 2;
 
+        return evpairs + odpairs;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int res = result(arr);
+        System.out.println(res);
+
+    }
 }
