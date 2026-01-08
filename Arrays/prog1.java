32e799c2735c1b34d20cc0309b9de5747aa112db
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-08 22:50:28 +0530

    2nd prog

--- Arrays/prog1.java
+++ Arrays/prog1.java
@@ -1,6 +1,36 @@
-// Check if Sum of Even-Indexed Elements > Sum of Odd-Indexed Elements
+package Arrays;
+//  Check if Sum of Even-Indexed Elements > Sum of Odd-Indexed Elements
 
 // Input: [1, 2, 3, 4, 5]
 // Even indices → arr[0] + arr[2] + arr[4] = 1 + 3 + 5 = 9
 // Odd indices → arr[1] + arr[3] = 2 + 4 = 6
 // Output: true
+import java.util.*;
+
+class prog1 {
+
+    public static boolean result(int[] arr) {
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                sum1 += arr[i];
+            } else {
+                sum2 += arr[i];
+            }
+        }
+        return sum1 > sum2;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        boolean res = result(arr);
+        System.out.println(res);
+
+    }
+}
---
+++ Arrays/prog2.java
@@ -0,0 +1,19 @@
+package Arrays;
+// Check if Any Two Numbers Sum Up to K
+
+// Given:
+// an integer array arr
+// an integer K
+// Return true if any two different elements in the array sum to K.
+// Otherwise return false.
+
+// Input:
+// arr = [2, 7, 11, 15]
+// K = 9
+
+// Output: true
+// Explanation: 2 + 7 = 9
+
+public class prog2 {
+
+}
