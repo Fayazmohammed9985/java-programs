54cc125434f48ede508332fdaaeb04e3bb73cf4f
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-08 23:14:39 +0530

    2nd prog

--- Arrays/prog2.java
+++ Arrays/prog2.java
@@ -19,7 +19,7 @@ import java.util.Scanner;
 public class prog2 {
     public static boolean result(int arr[], int k) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == k) {
                     return true;
                 }
