9cc974ae005c7e8043dfab80ecdbc45251fcd922
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-08 22:30:21 +0530

    1st prog

---
+++ Arrays/prog1.java
@@ -0,0 +1,6 @@
+// Check if Sum of Even-Indexed Elements > Sum of Odd-Indexed Elements
+
+// Input: [1, 2, 3, 4, 5]
+// Even indices → arr[0] + arr[2] + arr[4] = 1 + 3 + 5 = 9
+// Odd indices → arr[1] + arr[3] = 2 + 4 = 6
+// Output: true
