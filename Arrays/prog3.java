6484aaae000dcc0fc3f0d265636d70a0293413f5
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-08 23:49:51 +0530

    3nd prog

---
+++ Arrays/prog3.java
@@ -0,0 +1,13 @@
+package Arrays;
+// Count Pairs Whose XOR Is Even
+
+// Input:  [1, 2, 3, 4]
+// Pairs:
+// (1,3) → odd ^ odd = even
+// (2,4) → even ^ even = even
+
+// Output: 2
+
+public class prog3 {
+
+}
