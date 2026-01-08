492e94ab318ddfc7baddaaedfe534a015282e09c
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-08 23:04:27 +0530

    2nd prog

--- Arrays/prog2.java
+++ Arrays/prog2.java
@@ -1,6 +1,8 @@
 package Arrays;
 // Check if Any Two Numbers Sum Up to K
 
+import java.util.Scanner;
+
 // Given:
 // an integer array arr
 // an integer K
@@ -15,5 +17,27 @@ package Arrays;
 // Explanation: 2 + 7 = 9
 
 public class prog2 {
+    public static boolean result(int arr[], int k) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] + arr[j] == k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        boolean res = result(arr, k);
+        System.out.println(res);
+
+    }
 }
