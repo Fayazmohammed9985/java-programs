4ce4cb405f135ae9d7644fb5b540a6a5e8bdc567
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-10 12:38:04 +0530

    4th  prog

--- Arrays/prog4.java
+++ Arrays/prog4.java
@@ -1,10 +1,35 @@
 package Arrays;
 // find the maximum product of any two different elements in the array.
 
+import java.util.Scanner;
+
 // Input:  [1, 2, 3, 4]
 // Output: 12
 // Explanation: 3 × 4
+import java.util.*;
 
 public class prog4 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] > max2 && max1 != arr[i]) {
+                max2 = arr[i];
+
+            }
+
+        }
+        System.err.println(max1 * max2);
+
+    }
 
 }
