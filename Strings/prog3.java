e256b7b1c7237c37be38e3441bdfe33f2558c933
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-10 23:56:08 +0530

    3rd  prog

--- Strings/prog3.java
+++ Strings/prog3.java
@@ -7,11 +7,21 @@ package Strings;
 
 //output
 //yes
+import java.util.*;
 
 public class prog3 {
     public static void main(String[] args) {
-        String a = "listen";
-        String b = "silent";
+        String s1 = "listen";
+        String s2 = "silent";
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("Yes-Anagram");
+        } else {
+            System.out.println("No,it is not an Anagram");
+        }
 
     }
 }
