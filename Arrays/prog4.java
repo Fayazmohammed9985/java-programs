81939ab490cef9dabe004d45002d0688d0f93f2b
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-10 12:20:56 +0530

    4th  prog

---
+++ Arrays/prog4.java
@@ -0,0 +1,10 @@
+package Arrays;
+// find the maximum product of any two different elements in the array.
+
+// Input:  [1, 2, 3, 4]
+// Output: 12
+// Explanation: 3 × 4
+
+public class prog4 {
+
+}
