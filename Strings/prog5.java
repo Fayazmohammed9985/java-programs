dd6c26d54f3c0670c4673ec6bf42e81f8e6ec73f
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 00:52:14 +0530

    5th  prog

--- Strings/prog5.java
+++ Strings/prog5.java
@@ -1,5 +1,7 @@
 package Strings;
 
+import java.util.HashMap;
+
 // find the maximum difference between the frequency of any two characters in the string.
 // Input:  "aabbbc"
 // Frequencies:
@@ -10,5 +12,17 @@ package Strings;
 // Output: 3 − 1 = 2
 
 public class prog5 {
+    public static int difference(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length() - 1; i++) {
+            char c = s.charAt(i);
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
 
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "aabbbc";
+        int res = difference(s);
+    }
 }
