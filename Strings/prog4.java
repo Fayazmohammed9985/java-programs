35ec8b4164c83434fb9105c62f2ae335b39e2124
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 00:32:48 +0530

    4th  prog

--- Strings/prog4.java
+++ Strings/prog4.java
@@ -9,13 +9,27 @@ import java.util.Scanner;
 // Output:
 // 3
 public class prog4 {
+    public static int countFandL(String[] s) {
+        int count = 0;
+        for (int i = 0; i < s.length; i++) {
+            String a = s[i];
+            if (a.charAt(0) == a.charAt(s[i].length() - 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.nextLine();
         String[] s = new String[n];
         for (int i = 0; i < n; i++) {
             s[i] = sc.nextLine();
-
         }
+        int res = countFandL(s);
+        System.out.println(res);
+
     }
 }
