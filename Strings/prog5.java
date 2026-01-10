ea0e21fa4982133f90d51873c890fa7919fb42d0
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 00:39:24 +0530

    5th  prog

---
+++ Strings/prog5.java
@@ -0,0 +1,14 @@
+package Strings;
+
+// find the maximum difference between the frequency of any two characters in the string.
+// Input:  "aabbbc"
+// Frequencies:
+// a → 2
+// b → 3
+// c → 1
+
+// Output: 3 − 1 = 2
+
+public class prog5 {
+
+}
