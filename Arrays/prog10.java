8f15e4a2c142d002c5a73819afd5a291615a4428
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-10 14:45:37 +0530

    10th  prog

---
+++ Arrays/prog10.java
@@ -0,0 +1,23 @@
+package Arrays;
+//Find the Majority Element
+
+// You are given an integer array arr of size N.
+// An element is called a majority element if it appears more than N / 2 times.
+
+// Input:
+// 7
+// 2 2 1 2 3 2 2
+
+// Output:
+// 2
+
+// Input:
+// 5
+// -1 -1 -1 2 3
+
+// Output:
+// -1
+
+public class prog10 {
+
+}
