b8356618fce92aed1777760c35e144acaffb042c
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 00:16:09 +0530

    4th  prog

--- Strings/prog4.java
+++ Strings/prog4.java
@@ -1,14 +1,21 @@
 package Strings;
 // Count Strings With Same First and Last Character
 
+import java.util.Scanner;
+
 // Input:
 // ["abc", "aba", "a", "xyz", "aa"]
 
 // Output:
 // 3
-
 public class prog4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.nextLine();
 
+        }
     }
 }
