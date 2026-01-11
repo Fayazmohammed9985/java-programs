4cde5a2591329720d81d5e36a5888af9680f78e0
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-12 00:23:07 +0530

    10th prog

---
+++ Strings/prog10.java
@@ -0,0 +1,17 @@
+package Strings;
+// Count Strings Where Consonant Count Is a Prime Number
+
+// Input:
+// ["hello", "bcdf", "aeiou", "strength"]
+
+// Output:
+// 2
+
+// Input:
+// ["abc", "bcd", "pqrs"]
+// Output:
+// 2
+
+public class prog10 {
+
+}
