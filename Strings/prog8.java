53cc66ddf38e0c563de723021036d6504bbc74d3
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 23:28:37 +0530

    9th prog

--- Strings/prog8.java
+++ Strings/prog8.java
@@ -11,7 +11,28 @@ package Strings;
 // Input: "aabb"
 // Output: true
 // Explanation: "abba" is a palindrome.
+import java.util.*;
 
 public class prog8 {
+    public static boolean isPalidrome(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int count = 0;
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+        for (int num : hm.values()) {
+            if (num % 2 != 0)
+                count++;
+        }
+        if (count <= 1) {
+            return true;
+        }
+        return false;
+    }
 
+    public static void main(String args[]) {
+        String s = "aab";
+        System.out.print(isPalidrome(s));
+    }
 }
---
+++ Strings/prog9.java
@@ -0,0 +1,13 @@
+package Strings;
+// Longest Substring Containing Only Consonants
+
+// Input:  "bcdAEfg"
+// Output: 3
+
+// Explanation:
+// "bcd" → 3
+// "fg" → 2
+
+public class prog9 {
+
+}
