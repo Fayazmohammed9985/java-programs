954665ecd21fbaf29b076aa79f688f9ade093c58
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 16:45:35 +0530

    7th prog

--- Strings/prog7.java
+++ Strings/prog7.java
@@ -1,6 +1,8 @@
 package Strings;
 // First Non-Repeating Character in a String
 
+import java.util.HashMap;
+
 // Input:  "aabbbcde"
 // Output: c
 
@@ -11,5 +13,26 @@ package Strings;
 // Output: -1
 
 public class prog7 {
+    public static void main(String[] args) {
+        String s = "swiss";
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        boolean found = false;
+        for (int j = 0; j < s.length(); j++) {
+            if (hm.get(s.charAt(j)) == 1) {
+                System.out.print(s.charAt(j));
+                found = true;
+                break;
+            }
+
+        }
+        if (!found) {
+            System.out.print(-1);
+
+        }
+    }
 
 }
