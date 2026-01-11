47db9258f433003cc84bfff8125281b53626b1b4
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 23:59:34 +0530

    9th prog

--- Strings/prog9.java
+++ Strings/prog9.java
@@ -9,5 +9,27 @@ package Strings;
 // "fg" → 2
 
 public class prog9 {
+    public static int countCons(String s) {
+        int maxLength = 0;
+        int currLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetter(ch) && !"AEIOUaeiou".contains(String.valueOf(ch))) {
+                currLength++;
+            } else {
+                currLength = 0;
+            }
+            maxLength = Math.max(currLength, maxLength);
+
+        }
+        return maxLength;
+
+    }
+
+    public static void main(String args[]) {
+        String s = "bcdAEfg";
+        int res = countCons(s);
+        System.out.println(res);
+    }
 
 }
