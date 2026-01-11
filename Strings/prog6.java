0a45ab28c02f42ef4128b34177f83f71589debaf
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 16:06:15 +0530

    6th prog

--- Strings/prog6.java
+++ Strings/prog6.java
@@ -1,6 +1,8 @@
 package Strings;
 // Count Strings With More Vowels Than Consonants
 
+import java.util.Scanner;
+
 // Input:
 // ["hello", "aeiou", "bcdf", "education"]
 
@@ -8,5 +10,38 @@ package Strings;
 // 2
 
 public class prog6 {
+    String vowels = "AEIOUaeiou";
 
+    public static int countmethod(String[] s) {
+        int count = 0;
+        String vowels = "AEIOUaeiou";
+        for (String word : s) {
+            int vowelcount = 0;
+            int conscount = 0;
+            for (int i = 0; i < word.length(); i++) {
+                if (vowels.contains(String.valueOf(word.charAt(i)))) {
+                    vowelcount++;
+                } else {
+                    conscount++;
+                }
+            }
+            if (vowelcount > conscount) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.nextLine();
+        }
+        int res = countmethod(s);
+        System.out.println(res);
+    }
 }
