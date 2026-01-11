1cee752524f81dbeaec339a83554ffe62d15d1fd
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-11 16:49:12 +0530

    8th prog

---
+++ Strings/prog8.java
@@ -0,0 +1,17 @@
+package Strings;
+// Check if Rearranging Characters Can Form a Palindrome
+
+// Input: "aab"
+// Output: true
+// Explanation: "aba" is a palindrome.
+
+// Input: "abc"
+// Output: false
+
+// Input: "aabb"
+// Output: true
+// Explanation: "abba" is a palindrome.
+
+public class prog8 {
+
+}
