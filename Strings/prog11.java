050dc9f8182659763d3002246849cffd1324aa5b
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-13 00:06:14 +0530

    11th prog

---
+++ Strings/prog11.java
@@ -0,0 +1,13 @@
+package Strings;
+
+// Remove Duplicate Characters to Form the Lexicographically Smallest String
+// Input:  "bcabc"
+// Output: "abc"
+
+// Explanation:
+// Remove duplicates
+// Smallest lexicographical order → "abc"
+
+public class prog11 {
+
+}
