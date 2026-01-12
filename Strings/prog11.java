b141be8f0d933d29540766424ae1ed0fe7a53117
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-13 01:10:35 +0530

    11th prog

--- Strings/prog11.java
+++ Strings/prog11.java
@@ -1,5 +1,7 @@
 package Strings;
 
+import java.util.HashSet;
+
 // Remove Duplicate Characters to Form the Lexicographically Smallest String
 // Input:  "bcabc"
 // Output: "abc"
@@ -8,6 +10,25 @@ package Strings;
 // Remove duplicates
 // Smallest lexicographical order → "abc"
 
+// Input:  "cbacdcbc"
+// Output: "acdb"
+
 public class prog11 {
+    public static String smallstring(String s){
+        HashSet<Charecter> hs=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            hs.add(s.charAt(i));
+        }
+        for(char ch:hs){
+            if()
+        }   
+
+    }
+
+    public static void main(String[] args) {
+        String s = "bcdAEfg";
+        String res = smallstring(s);
+        System.out.println(res);
+    }
 
 }
