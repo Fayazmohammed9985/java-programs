568b0ec0f770ee0410884f6f0cf5241dd576ff67
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-13 02:29:01 +0530

    11th prog

--- Strings/prog11.java
+++ Strings/prog11.java
@@ -1,6 +1,8 @@
 package Strings;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Stack;
 
 // Remove Duplicate Characters to Form the Lexicographically Smallest String
 // Input:  "bcabc"
@@ -15,11 +17,33 @@ import java.util.HashSet;
 
 public class prog11 {
     public static String smallstring(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        HashSet<Character> finalstring = new HashSet<>();
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.get(c) - 1);
+            if (finalstring.contains(c))
+                continue;
+            while (!stack.isEmpty() && stack.peek() > c && hm.get(stack.peek()) > 0) {
+                finalstring.remove(stack.pop());
+            }
+            stack.push(c);
+            finalstring.add(c);
+
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
 
     }
 
     public static void main(String[] args) {
-        String s = "bcdAEfg";
+        String s = "cbacdcbc";
         String res = smallstring(s);
         System.out.println(res);
     }
