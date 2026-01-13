27a37155584a6528c73bc7137526a0f05f1d6763
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-14 00:30:55 +0530

    12th prog

--- Strings/prog12.java
+++ Strings/prog12.java
@@ -1,6 +1,8 @@
 package Strings;
 // Validate Parentheses String With One Allowed Mismatch
 
+import java.util.Scanner;
+
 // Input: "()())"
 // Output: true
 
@@ -8,5 +10,33 @@ package Strings;
 // Output: false
 
 public class prog12 {
+    public static boolean valid(String s) {
+        int count = 0;
+        int one_chance = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                count++;
+            } else if (ch == ')') {
+                count--;
+            }
+            if (count < 0 && one_chance == 1) {
+                count++;
+                one_chance = 0;
 
+            } else if (count < 0 && one_chance == 0) {
+                return false;
+            }
+
+        }
+        return (count == 0 || count == 1);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        boolean res = valid(s);
+        System.out.println(res);
+    }
 }
