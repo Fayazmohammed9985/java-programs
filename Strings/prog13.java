1b775292416271e6b1d19b9653f8c91c4a9b1bbb
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-14 00:37:28 +0530

    13th prog

---
+++ Strings/prog13.java
@@ -0,0 +1,17 @@
+package Strings;
+// Maximum Frequency Difference in a String
+
+// Input:  "aabbbc"
+// Frequencies:
+// a → 2
+// b → 3
+// c → 1
+
+// Output: 3 − 1 = 2
+
+// Input:  "aaaa"
+// Output: 0
+
+public class prog13 {
+
+}
