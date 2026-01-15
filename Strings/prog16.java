c4c3e98b35e40b38a51b7c96b0a9a2a631fd5b60
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-16 01:39:33 +0530

    16 prog

--- Strings/prog16.java
+++ Strings/prog16.java
@@ -14,20 +14,26 @@ public class prog16 {
 
     public static void main(String[] args) {
         String[] arr = { "abc", "aa", "ab", "aabbcc" };
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                int d1 = distinctCount(arr[j]);
-                int d2 = distinctCount(arr[j + 1]);
-                if (d1 > d2) {
-                    String temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+        // int n = arr.length;
+        // for (int i = 0; i < n - 1; i++) {
+        // for (int j = 0; j < n - i - 1; j++) {
+        // int d1 = distinctCount(arr[j]);
+        // int d2 = distinctCount(arr[j + 1]);
+        // if (d1 > d2) {
+        // String temp = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = temp;
+        // }
+        // }
+        // }
+        List<String> list = Arrays.asList(arr);
 
-        for (String s : arr) {
+        Collections.sort(list, (s1, s2) -> {
+            int d1 = distinctCount(s1);
+            int d2 = distinctCount(s2);
+            return d1 - d2;
+        });
+        for (String s : list) {
             System.out.print(s + " ");
         }
     }
