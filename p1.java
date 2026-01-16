8fe5abf1a39cc3ea98e52c4b80999de6fcc381d5
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-16 23:16:22 +0530

    p1

---
+++ p1.java
@@ -0,0 +1,17 @@
+// Maximum Number of People Using Lift Without Exceeding Capacity
+// There is a lift in your society that can accommodate a maximum weight of X units.
+// The weights of the people planning to use the lift are given in an integer array A of size N.
+// Your task is to find and return an integer value representing the maximum number of people that can use the lift together without exceeding the lift’s weight capacity.
+
+// input1: An integer N, representing the number of people
+// input2: An integer X, representing the maximum weight capacity of the lift
+// input3: An integer array A of size N, representing the weights of people
+
+// input1 = 3
+// input2 = 9
+// input3 = {5, 1, 5}
+// Output
+// 2
+public class p1 {
+
+}
