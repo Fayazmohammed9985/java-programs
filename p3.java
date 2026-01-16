818c9540b631507830e4c64e218ce2a3b7a3d948
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-17 00:39:27 +0530

    p3

---
+++ p3.java
@@ -0,0 +1,16 @@
+// Factor Finder:
+// Write a program to find the factors of a given number. If the input provided is negative,
+// ignore the sign and provide the output. If the input is zero, the output should be "No
+// Factors."
+// Approach: Take an integer input from the user. If the number is zero, display "No
+// Factors." If negative, ignore the sign by taking the absolute value. Then, find and print all
+// numbers that divide the given number exactly without a remainder
+
+// Input
+// 12
+
+// Output
+// 1 2 3 4 6 12
+public class p3 {
+
+}
