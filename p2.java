4e3550fd1743c2b7f06e6358dbd1d7b71e3dc775
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-16 23:43:02 +0530

    p2

---
+++ p2.java
@@ -0,0 +1,28 @@
+// Write a program to calculate the fuel consumption of a truck.
+
+// The program should ask the user to enter:
+
+// the quantity of diesel required to fill the tank, and
+
+// the distance covered until the tank goes dry.
+
+// The program must calculate and display the fuel consumption in the format liters per 100 kilometers.
+// Then convert the same result to the U.S. style of miles per gallon and display it.
+
+// If the quantity of diesel or the distance covered is zero or negative, display "Invalid Input".
+
+// Input 1: A floating-point value representing the quantity of diesel (in liters)
+
+// Input 2: A floating-point value representing the distance covered (in kilometers)
+
+// Sample Input 1
+// 20
+// 150
+
+// Sample Output 1
+// 13.33
+// 17.64
+
+public class p2 {
+
+}
