2e92b2c46fc04e18d8a4e3368a6d701c33dbe707
Fayazmohammed9985 <fayazmohammed9985@gmail.com>
2026-01-17 00:51:52 +0530

    p4

---
+++ p4.java
@@ -0,0 +1,37 @@
+// Ticket Booking Discount:
+// Develop a program to find the total cost of tickets based on a discount scheme. There is a
+// 10% discount on the total cost of tickets when there is a bulk booking of more than 20
+// tickets, and a discount of 2% on the total cost of tickets if a special coupon card is
+// submitted. The cost of the k class ticket is Rs.75 and q class is Rs.150. Refreshments can
+// also
+// be opted by paying an additional of Rs. 50 per member. If the number of tickets is less than
+// 5 or more than 40, display "Minimum of 5 and Maximum of 40 Tickets". If the ticket type is
+// given a value other than 'k' or 'q'
+// , the output should be "Invalid Input". The ticket cost should
+// be printed exactly to two decimal places.
+// Approach: Take inputs: number of tickets, ticket type (k or q), whether refreshments are
+// needed, and if a coupon is submitted. Validate ticket number and ticket type. Calculate base
+// cost, add refreshment charges if needed, apply a 10% discount if tickets >20, then apply a
+// 2% discount if a coupon is available. Finally, print the total cost to two decimal places.
+
+// Input
+// 25
+// k
+// y
+// y
+
+// Output
+// 1953.75
+
+// Input
+// 3
+// q
+// n
+// n
+
+// Output
+// Minimum of 5 and Maximum of 40 Tickets
+
+public class p4 {
+
+}
